53ef4c8e8b7218805a7eab29a3d91a36b1cf3fcb
GitHub Action <action@github.com>
2024-05-24 06:00:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/flymantou/ippttemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/flymantou/ippttemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.flymantou.ippttemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
